8beac5db0bbeb94f63eae33fe01c6f3bda822a1b
Mostafa Ahmed <mostafafikry97@gmail.com>
2026-02-24 18:49:24 +0200

    ✨ feat(dotenv): added dotenvconfig to inde.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import dotenv from "dotenv";
 import express from "express";
 const app = express();
+dotenv.config();
 
 app.use(express.json());
 app.get("/", (req, res) => res.send("Hello world !!"));
